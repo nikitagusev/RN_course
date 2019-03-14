1d23147bdf3c92a99115f75fa25641a0ad431b6a
nikitagusev <nikita_gusev@hotmail.ca>
2019-03-14 00:25:31 -0400

    changed auth screen

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,21 +1,33 @@
-import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { Component } from "react";
+import { View, Text, Button, TextInput, StyleSheet } from "react-native";
 
-import startMainTabs from '../MainTabs/startMainTabs';
+import startMainTabs from "../MainTabs/startMainTabs";
 
 class AuthScreen extends Component {
-    loginHandler = () => {
-        startMainTabs();
-    }
+  loginHandler = () => {
+    startMainTabs();
+  };
 
-    render () {
-        return (
-            <View>
-                <Text>Auth Screen</Text>
-                <Button title="Login" onPress={this.loginHandler}/>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Please Log In</Text>
+        <Button title="Switch to Login" />
+        <TextInput placeholder="Your E-Mail Address" />
+        <TextInput placeholder="Password" />
+        <TextInput placeholder="Confirm Password" />
+        <Button title="Submit" onPress={this.loginHandler} />
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
 
 export default AuthScreen;
