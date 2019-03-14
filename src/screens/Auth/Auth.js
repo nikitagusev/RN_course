9f610471e3cc4af2300e6e3c9b05dc193cfca17d
nikitagusev <nikita_gusev@hotmail.ca>
2019-03-14 00:39:52 -0400

    started secion 7 lesson 88

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -13,9 +13,11 @@ class AuthScreen extends Component {
       <View style={styles.container}>
         <Text>Please Log In</Text>
         <Button title="Switch to Login" />
-        <TextInput placeholder="Your E-Mail Address" />
-        <TextInput placeholder="Password" />
-        <TextInput placeholder="Confirm Password" />
+        <View style={styles.inputContainer}>
+          <TextInput placeholder="Your E-Mail Address" style={styles.input}/>
+          <TextInput placeholder="Password" style={styles.input}/>
+          <TextInput placeholder="Confirm Password" style={styles.input}/>
+        </View>
         <Button title="Submit" onPress={this.loginHandler} />
       </View>
     );
@@ -27,6 +29,16 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
+  },
+  inputContainer: {
+    width: "80%"
+  },
+  input: {
+    width: "100%",
+    borderWidth: 1,
+    borderColor: "#eee",
+    padding: 5,
+    margin: 8
   }
 });
 
