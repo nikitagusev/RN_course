474381c52141e7436683f64af9a7d386eb1d42a2
nikitagusev <nikita_gusev@hotmail.ca>
2019-03-14 00:20:25 -0400

    initial commit

---
+++ android/app/src/main/java/com/rn_pre/MainActivity.java
@@ -0,0 +1,17 @@
+package com.rn_pre;
+
+
+import com.reactnativenavigation.controllers.SplashActivity;
+
+
+public class MainActivity extends SplashActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+
+    protected String getMainComponentName() {
+        return "rn_pre";
+    }
+}
